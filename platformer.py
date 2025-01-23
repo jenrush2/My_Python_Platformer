d43a8aa302f6072dbe28ae1f2c23a393263e5149
Jennifer Rush <jennifer.rush2@gmail.com>
2025-01-23 11:12:26 -0700

    finishes ability to run empty game then exit

--- platformer.py
+++ platformer.py
@@ -16,7 +16,22 @@ PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main(window):
-    pass
+    clock = pygame.time.Clock()
+
+    run = True
+    
+    while run:
+        #regulate frame rate across devices
+        clock.tick(FPS)
+
+        #allow exiting the game by clicking the x on the top right
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
 
 #only call the main function and call the game if we run the file directly
 if __name__ == "__main__":
